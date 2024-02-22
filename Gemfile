50c4dcc21daf51492c63a67ef99e378341fb487d
ng <ng_zi_jie@hotmail.com>
2024-02-22 16:33:34 +0800

    complete

--- Gemfile
+++ Gemfile
@@ -57,6 +57,8 @@ group :development do
   # gem "spring"
 
   gem "error_highlight", ">= 0.4.0", platforms: [:ruby]
+
+  gem "faker"
 end
 
 gem 'rspec-rails', group: [ :test ]
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "posts#index"
+  resources :restaurants, except: %i[update edit destroy] do 
+    resources :reviews, except: %i[update edit destroy]
+  end
 end
